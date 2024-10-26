4608729f36a83c73ac75b9b10387af87e2da8d0e
muhammadIdhamMaarif <muhammadidhammaarif@gmail.com>
2024-10-26 16:57:44 +0700

    Create Menghitung_Luas_Segitiga.cpp

---
+++ Menghitung_Luas_Segitiga.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    float a, b;
+    cin >> a >> b;
+    cout << a * b * 1/2;
+    return 0;
+}
